8111fddb6276886d565b348cf0a76db29765982b
nathan meyer <nmeyer@nathans-MacBook-Air.local>
2019-05-20 10:48:51 -0700

    updates

--- javascript/liri.js
+++ javascript/liri.js
@@ -29,10 +29,15 @@ var spotify = new Spotify({
   secret: "2817b4a1ea7a4412ad4e19baa8908341"
 
 });
+//function definition wrap all code below
+
+// function spotifySong(value) {
+
  if (command === "spotify-this-song" ) {
     console.log("browsing song");
  
-    spotify.search({ type: 'track', query: search }, function(err, data) {
+    spotify.search({
+         type: 'track', query: search }, function(err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
@@ -55,94 +60,115 @@ var command = process.argv[2];
 console.log(command);
 var search = process.argv[3];
 console.log(search);
-console.log(process.argv)
+console.log(process.argv[0])
+
+// function concertThis(value) {
 
 if (command === "concert-this") {
 
     var axios = require('axios')
 
-    // SEVERAL ATTEMPTS
-    // this.venue = venue; //failed
-    // var venue; // failed
-    
         axios.get("https://rest.bandsintown.com/artists/" + search + "/events?app_id=codingbootcamp")
-        // axios.get("https://rest.bandsintown.com/artists/" + search +  venue + "/events?app_id=codingbootcamp")
 
         .then(function(response) {
-console.log(response);
-// console.log(response.data);
+        // console.log(response);
+        // console.log(response.data);
+
+console.log("______________________________________");
+console.log("______________________________________");
+console.log("_________BANDSINTOWN DATABASE_________");
+console.log("______________________________________");
+console.log("______________________________________");
 
             if(response.data[0].venue != undefined) {
-                console.log(" Venue: " + response.data[0].venue.name);
+
+                console.log(" Venue : " + response.data[0].venue.name);
                 console.log(" Location : " + response.data[0].venue.city);
                 var datetime = moment(response.data[0].datetime)
                 console.log(response.data[0].datetime);
                 
                 console.log("Date of the event") + datetime.format("MM/DD/YYYY");
-console.log(JSON.stringify,null,2);
-
-                // for (var i = 0; i < venue.length; i++) {
-                // }
+                // console.log(JSON.stringify,null,2);
             }
             }).catch(function(err){console.log(err)})
 
             }
-            
-            
-            
-            
+
             /////////////////// OMDB SECTION
     
             var command = process.argv[2];
             console.log(command);
             var search = process.argv[3];
             console.log(search);
-    
+
+            // function movieThis(value) {
             if (command === "movies-this") {
 
                 var axios = require("axios");
+                // console.log(axios);
+                
 
-                axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy")
+                // axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy")
+                axios.get("http://www.omdbapi.com/?t=" + search + "&y=&plot=short&apikey=trilogy")
+            
+
                 .then(function(response) {
+                    // console.log(response);
+                    if(search != " ") {
+                       search = "Mr. Nobody";
+                    }
+                    
+                console.log("______________________________________");
+                console.log("______________________________________");
+                console.log("_____________OMDB DATABASE____________");
+                console.log("______________________________________");
+                console.log("______________________________________");
                 console.log(" Title of movie is : " + response.data.Title);
                 console.log(" Year : " + response.data.Year);
                 console.log(" The movie's rating is : " + response.data.imdbRating);
                 console.log(" The Plot is based on : " + response.data.Plot);
-                console.log(" Actors: " + resonse.data.Actors);
-                console.log(" RottenTomatoes" + response.data.tomatoRating);
-            }
-                )}
-        
-                // console.log("Mr.Nobody");
+                console.log(" Language : " + response.data.Language);
+                console.log(" County : " + response.data.Country);
+                console.log(" Actors: " + response.data.Actors);
+                console.log(" Rotten Tomatoes score : " + response.data.Ratings[1].Value);
+            });
             
-       
-    
+        }
+        // else  { if (command === " ") {
+        //     console.log("Mr.Nobody");
+        // }   
+        
+         ////////////// DO-WHAT-IF-SAYS
 
-            // console.log(err);
-            // console.log("No Results founds.");
-            // console.log("movies database");
-// ))
-//         });
-    
+            // var command = process.argv[2];
+            // console.log(command);
+            // var search = process.argv[3];
+            // console.log(search);
+
+
+            //     if (command === "do-what-it-says") {
+            //         console.log("do-what-it-says"); 
+
+            //         switch (command) {
+            //             case "spotify-this-song":
+            //             spotifySong(value)
+            //             break;
+            //             case "concert-this":
+            //             concertThis(value)
+            //             break;
+            //             case "movies-this":
+            //             movieThis(value)
+            //             break;
+            //             case "do-what-it-says":
+            //             doThis(value)
+            //             break;
+
+            //         };
+            //     }
                 
-        
             
-       
-        
+                    // function command (logText) {
+                    //     log.info(logText);
+                    //     console.log(logText);
     
-
-   
-    
-    
-
-    
-
-        
-// if (command === "movies-this") {
-//     console.log("movies database");
-
-
-// }
-// if (command === "do-what-it-says") {
-//     console.log("simon says"); 
-//     }
+                    // }
